fcaf8938e9d0813a05e9746231a049ce7f934b3e
scstech85 <samuele.capobianco@unifi.it>
2017-09-22 02:51:04 +0200

    [add] viz script

--- tools/create_attention_graph.py
+++ tools/create_attention_graph.py
@@ -15,8 +15,18 @@ score = np.squeeze(np.load(args.path + '_score.npy'))
 attention = np.squeeze(np.load(args.path + '_attention.npy'))
 gt = np.squeeze(np.load(args.path + '_gt.npy'))
 
-cmap = colors.ListedColormap(['red', 'blue'])
+cmap = matplotlib.cm.get_cmap('Spectral')
+norm = matplotlib.colors.Normalize(vmin=0, vmax=100)
+fig, ax = plt.subplots()
 
+attention = np.array([item[:10]/np.sum(item[:10])*100 for item in attention])
 
+ax.grid(which='major', axis='both', linestyle='-', color='k', linewidth=1)
+ax.set_xticks(np.arange(10, len(review[0].split(' ')[:10]), 1))
+ax.set_yticks(np.arange(1, attention.shape[1], 1));
+
+plt.imshow(attention[0,:,:len(review[0].split(' ')[:10])], norm=norm, cmap=cmap)
+
+plt.savefig('/tmp/tmp.pdf')
 
 
